875c3c7857a8943eb6b806d901978712286380b0
Christine Mumbi <christinemumbi286@gmail.com>
2025-09-18 15:06:43 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback){
+    callback();
+}
+function returnsANamedFunction(){
+    function namedFunc(){
+        console.log("A named function")
+    }
+    return namedFunc;
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+        console.log("I'm an anonymous function!")
+    };
+}
